8a83a5a514c718eabe7c7bf43b5d1b288e9ed899
lionelray <lionel.r.liu@gmail.com>
2017-10-16 14:35:38 +0800

    Update common.py

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -62,6 +62,7 @@ SITES = {
     'panda'            : 'panda',
     'pinterest'        : 'pinterest',
     'pixnet'           : 'pixnet',
+    'pornhub'          : 'pornhub',
     'pptv'             : 'pptv',
     'qingting'         : 'qingting',
     'qq'               : 'qq',
